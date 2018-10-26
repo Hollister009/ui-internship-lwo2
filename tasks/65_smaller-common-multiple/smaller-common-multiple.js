ae08ea43303c8e2ea4d19abf3e0809c703018a6e
Andriy Zhukevych <reactive.adnrew@yahoo.com.ua>
2018-10-26 14:14:29 +0300

    improvments of 65, and small fix

--- tasks/65_smaller-common-multiple/smaller-common-multiple.js
+++ tasks/65_smaller-common-multiple/smaller-common-multiple.js
@@ -1,16 +1,22 @@
 export const smallestCommons = (arr) => {
-  const rangeArr = [Math.min(...arr)];
+  const minNum = Math.min(...arr);
   const maxNum = Math.max(...arr);
-  let num = rangeArr[0] + 1;
+  const rangeArr = [minNum];
+  let num = minNum + 1;
+  let curCheckNum = maxNum * 2;
+
   while (num <= maxNum) {
     rangeArr.push(num);
     num++;
   }
-  let curN = maxNum * 2;
-  let condition = rangeArr.every((el) => el % curN === 0);
-  while (!condition) {
-    curN += maxNum;
-    condition = rangeArr.every((el) => curN % el === 0);
+
+  while (!isCommonNumberMult(rangeArr, curCheckNum)) {
+    curCheckNum += maxNum;
   }
-  return curN;
+
+  function isCommonNumberMult(array, checkNum) {
+    return array.every((el) => checkNum % el === 0);
+  }
+
+  return curCheckNum;
 };
